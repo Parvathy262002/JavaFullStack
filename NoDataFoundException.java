38772799d8b69812a737ab5cb90468ff6df1cc77
Parvathy262002 <142981870+Parvathy262002@users.noreply.github.com>
2023-09-20 10:44:55 +0530

    Add files via upload

---
+++ NoDataFoundException.java
@@ -0,0 +1,10 @@
+package com.springrest.SpringRestAPI.exception;
+
+public class NoDataFoundException extends Exception{
+	
+	public NoDataFoundException(String message)
+	{
+		super(message);
+	}
+
+}
